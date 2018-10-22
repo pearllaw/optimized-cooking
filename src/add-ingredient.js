e3cf785c0ad216ae33ad3b34dae80b4b2473d0f8
Pearl Law <pearllynnelaw@gmail.com>
2018-10-22 16:31:01 -0700

    When user adds an item, it is sent to the client server database

---
+++ src/add-ingredient.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/Add'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+  container: {
+    marginTop: 150,
+    textAlign: 'center'
+  },
+  input: {
+    display: 'inline-block',
+    margin: 2 + 'rem'
+  },
+  textField: {
+    width: 600
+  },
+  button: {
+    marginTop: 21,
+    marginLeft: 25
+  }
+}
+
+class AddIngredient extends Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const newIngredient = {
+      ingredient: e.target['ingredient'].value
+    }
+    this.props.addIngredient(newIngredient)
+    e.target.reset()
+  }
+
+  render() {
+    const { classes } = this.props
+    return (
+      <div className={classes.container}>
+        <Typography variant="h3" align="center">What's in Your Fridge?</Typography>
+        <form className={classes.input} onSubmit={this.handleSubmit}>
+          <TextField
+            className={classes.textField}
+            id="ingredient"
+            label="Add an ingredient"
+            margin="normal"
+            variant="outlined"
+            required />
+          <Button
+            className={classes.button}
+            type="submit"
+            variant="fab"
+            mini
+            color="primary"
+            aria-label="add">
+            <AddIcon/>
+          </Button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(AddIngredient)
