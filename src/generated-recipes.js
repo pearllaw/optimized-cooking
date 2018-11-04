7bf7ac925f2fb7bb9a51c66541f7fb609d318c05
Pearl Law <pearllynnelaw@gmail.com>
2018-11-04 15:32:28 -0800

    Merged recipe list component into generated recipes class

--- src/generated-recipes.js
+++ src/generated-recipes.js
@@ -1,7 +1,30 @@
 import React, {Component} from 'react'
-import RecipeList from './recipe-list'
+import { GridList, GridListTile, GridListTileBar, Typography, withStyles } from '@material-ui/core/'
 
-export default class GeneratedRecipes extends Component {
+const styles = {
+  title: {
+    marginTop: 100,
+    marginBottom: 80
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden'
+  },
+  gridList: {
+    width: 1200,
+    height: 800,
+    transform:'translateZ(0)'
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  }
+}
+
+class GeneratedRecipes extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -24,9 +47,31 @@ export default class GeneratedRecipes extends Component {
 
   render() {
     const { recipes } = this.state
+    const { classes } = this.props
     if(!recipes) return null
     return (
-      <RecipeList recipes={recipes}/>
+      <div>
+      <Typography variant="h3" align="center" className={classes.title}>What Looks Good?</Typography>
+      <div className={classes.container}>
+        <GridList cols={3} cellHeight={'auto'} className={classes.gridList}>
+          {recipes.map(tile => (
+            <GridListTile key={tile.image}>
+              <a href={`#view-recipe?id=${tile.id}`}><img src={tile.image}
+                alt={tile.title}
+                id={tile.id}
+                width="396px"
+                height="235px"/></a>
+              <GridListTileBar
+                title={tile.title}
+                titlePosition="top"
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+      </div>
     )
   }
 }
+
+export default withStyles(styles)(GeneratedRecipes)
--- src/recipe-list.js
+++ src/recipe-list.js
@@ -1,51 +1,10 @@
 import React from 'react'
-import { GridList, GridListTile, GridListTileBar, Typography, withStyles } from '@material-ui/core/'
 
-const styles = {
-  title: {
-    marginTop: 100,
-    marginBottom: 80
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden'
-  },
-  gridList: {
-    width: 1200,
-    height: 800,
-    transform:'translateZ(0)'
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  }
-}
+
 
 function RecipeList({ classes, recipes }) {
   return (
-      <div>
-      <Typography variant="h3" align="center" className={classes.title}>What Looks Good?</Typography>
-      <div className={classes.container}>
-        <GridList cols={3} cellHeight={'auto'} className={classes.gridList}>
-          {recipes.map(tile => (
-            <GridListTile key={tile.image}>
-              <a href={`#view-recipe?id=${tile.id}`}><img src={tile.image}
-                alt={tile.title}
-                id={tile.id}
-                width="396px"
-                height="235px"/></a>
-              <GridListTileBar
-                title={tile.title}
-                titlePosition="top"
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-      </div>
+      <div></div>
     )
 }
 
