752f997f8995b17ff8ef2e0bff6ce5b853a6d5f6
Pearl Law <pearllynnelaw@gmail.com>
2018-11-09 15:49:22 -0800

    Refactor code by creating a separate component for my-recipes to display in recipe collection container #12

--- src/containers/view-recipe-collection.js
+++ src/containers/view-recipe-collection.js
@@ -1,41 +1,8 @@
 import React, {Component} from 'react'
-import { withStyles, Grid, Typography, List, ListItemText, ButtonBase, Card, CardMedia, CardContent, CardHeader } from '@material-ui/core'
+import MyRecipes from '../components/my-recipes'
+import { Typography } from '@material-ui/core';
 
-const styles = {
-  container: {
-    marginTop: 100
-  },
-  list: {
-    width: 560,
-    borderRadius: '0.25rem'
-  },
-  card: {
-    display: 'flex',
-    margin: 15,
-    height: 100
-  },
-  content: {
-    flex: '1 0 auto',
-  },
-  details: {
-    display: 'flex',
-    alignItems: 'center',
-    width: 360
-  },
-  image: {
-    width: 150,
-  },
-  icon: {
-    fontSize: '120%',
-    fontWeight: 600,
-    opacity: 0.8,
-    padding: '10px',
-    position: 'absolute',
-    color: 'white'
-  }
-}
-
-class RecipeCollection extends Component {
+export default class RecipeCollection extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -61,42 +28,10 @@ class RecipeCollection extends Component {
   }
 
   render() {
-    const { classes } = this.props
     const { savedRecipes } = this.state
     return (
-      <Grid container
-        alignItems="center"
-        direction="column"
-        spacing={40}
-        className={classes.container}
-        >
-        <Typography variant="h3" align="center">My Recipes</Typography>
-        {savedRecipes.length > 0 &&
-        <Grid item>
-          <List className={classes.list}>
-          {savedRecipes.map(recipe => {
-          return <Card className={classes.card} key={recipe.recipeId}>
-              <i className={`material-icons ${classes.icon}`}
-                id={recipe.id}
-                onClick={this.deleteRecipe}>clear</i>
-              <CardMedia className={classes.image}
-                image={recipe.image}
-                title="My Saved Recipe" />
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <ButtonBase href={`#view-recipe?id=${recipe.recipeId}`}>
-                      <ListItemText primary={recipe.title} />
-                    </ButtonBase>
-                  </CardContent>
-                </div>
-            </Card>
-          })}
-        </List>
-        </Grid>
-        }
-      </Grid>
+      <MyRecipes savedRecipes={savedRecipes}
+        deleteRecipe={this.deleteRecipe}/>
     )
   }
 }
-
-export default withStyles(styles)(RecipeCollection)
