683cb6db07df49675d9118a69ad0035a52b4150a
Pearl Law <pearllynnelaw@gmail.com>
2018-11-10 09:56:14 -0800

    Change sizing of recipelist.js grid in generated recipes container #27

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { GridList, GridListTile, GridListTileBar, Typography, withStyles } from '@material-ui/core/'
 
-const styles = {
-  title: {
+const styles = theme => ({
+  heading: {
     marginTop: 100,
     marginBottom: 80
   },
@@ -12,23 +12,24 @@ const styles = {
     justifyContent: 'space-around',
     overflow: 'hidden'
   },
-  gridList: {
-    width: 1200,
-    height: 800,
-    transform:'translateZ(0)'
-  },
+  gridList: window.screen.availWidth < 760
+  ? { width: 300, height: 180, transform: 'translateZ(0)' }
+  : { width: 1200, height: 800, transform: 'translateZ(0)' },
+  image: window.screen.availWidth < 760
+  ? { width: '150px', height: '80px'}
+  : { width: '396px', height: '235px'},
   titleBar: {
     background:
       'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
       'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   }
-}
+})
 
 function RecipeList({ classes, recipes }) {
     if(!recipes) return <div>Loading...</div>
     return (
       <div>
-      <Typography variant="h3" align="center" className={classes.title}>What Looks Good?</Typography>
+      <Typography variant="h3" align="center" className={classes.heading}>What Looks Good?</Typography>
       <div className={classes.container}>
         <GridList cols={3} cellHeight={'auto'} className={classes.gridList}>
           {recipes.map(tile => (
@@ -36,8 +37,7 @@ function RecipeList({ classes, recipes }) {
               <a href={`#view-recipe?id=${tile.id}`}><img src={tile.image}
                 alt={tile.title}
                 id={tile.id}
-                width="396px"
-                height="235px"/></a>
+                className={classes.image}/></a>
               <GridListTileBar
                 title={tile.title}
                 titlePosition="top"
