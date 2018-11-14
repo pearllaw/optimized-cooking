d9b5cd4b529a1695b0db72fb8c179c79cd2050df
Pearl Law <pearllynnelaw@gmail.com>
2018-11-14 11:49:35 -0800

    Add nutritional info component to view recipe container #28

--- src/components/meals-by-cal.js
+++ src/components/meals-by-cal.js
@@ -2,33 +2,34 @@ import React from 'react'
 import { GridList, GridListTile, GridListTileBar, Typography, withStyles } from '@material-ui/core/'
 
 const styles = {
-  heading: {
-    marginTop: 100,
-    marginBottom: 80
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden'
-  },
-  gridList: window.screen.availWidth < 760
-  ? { width: 300, height: 180, transform: 'translateZ(0)' }
-  : { width: 1200, height: 800, transform: 'translateZ(0)' },
-  image: window.screen.availWidth < 760
-  ? { width: '150px', height: '80px'}
-  : { width: '396px', height: '235px'},
-  titleBar: {
-    background:
-      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  }
+//   heading: {
+//     marginTop: 100,
+//     marginBottom: 80
+//   },
+//   container: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//     overflow: 'hidden'
+//   },
+//   gridList: window.screen.availWidth < 760
+//   ? { width: 300, height: 180, transform: 'translateZ(0)' }
+//   : { width: 1200, height: 800, transform: 'translateZ(0)' },
+//   image: window.screen.availWidth < 760
+//   ? { width: '150px', height: '80px'}
+//   : { width: '396px', height: '235px'},
+//   titleBar: {
+//     background:
+//       'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+//       'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+//   }
 }
 
-function MealsByCal({ classes, recipes }) {
+function MealsByCal() {
     return (
-      <div>
-      <Typography variant="h3" align="center" className={classes.heading}>What Looks Good?</Typography>
+      <div>Hello</div>)
+      // <div>
+      {/* <Typography variant="h3" align="center" className={classes.heading}>What Looks Good?</Typography>
       <div className={classes.container}>
         <GridList cols={3} cellHeight={'auto'} className={classes.gridList}>
           {recipes.map(tile => (
@@ -45,8 +46,8 @@ function MealsByCal({ classes, recipes }) {
           ))}
         </GridList>
       </div>
-      </div>
-    )
+      </div> */}
+    // )
 }
 
 export default withStyles(styles)(MealsByCal)
