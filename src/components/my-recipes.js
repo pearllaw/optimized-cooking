86714fab79ebe00a9d673565bb8ee3a9151fd41c
Pearl Law <pearllynnelaw@gmail.com>
2018-11-30 12:46:58 -0800

    Add grid list component within Expansion Panel Details of accordion component that stores meal categories based on filtered arrays #36

--- src/components/my-recipes.js
+++ src/components/my-recipes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { withStyles, Grid, Typography, GridList, ListItemText, ButtonBase, Card, CardMedia, CardContent, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core'
+import { withStyles, Grid, Card, CardContent, Typography, GridList, GridListTile, GridListTileBar, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, createMuiTheme, MuiThemeProvider } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 const styles = theme => ({
@@ -15,49 +15,47 @@ const styles = theme => ({
     flexBasis: '33.33%',
     flexShrink: 0,
   },
-  // gridList: {
-  //   width: 500,
-  //   height: 450
-  // },
-  list: {
-    width: 560,
-    borderRadius: '0.25rem'
-  },
-  card: {
-    display: 'flex',
-    margin: 15,
-    height: 100,
-    width: 500
-  },
-  details: {
-    display: 'flex',
-    alignItems: 'center'
-  },
-  content: {
-    display: 'inline-block',
-    width: 300
+  gridList: {
+    maxHeight: 450,
+    width: 990,
+    overflow: 'scroll'
   },
   image: {
-    width: 200,
+    width: 946.39/3
   },
   icon: {
-    fontSize: '120%',
+    borderRadius: '50%',
+    border: '1px solid',
     fontWeight: 600,
-    opacity: 0.8,
-    padding: '5px',
-    position: 'relative',
-    bottom: '30px',
-    right: '10px'
+    padding: '3px',
+    fontSize: '12px',
+    marginRight: '10px',
+    color: 'white'
   },
   messageContainer: {
-    marginTop: 100,
-    border: '1px solid silver',
-    borderRadius: '1rem',
-    boxShadow: 'none',
-    padding: 30
+    padding: 10
   },
   message: {
     fontSize: '1.2rem'
+  }
+})
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiCard: {
+      root: {
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }
+    },
+    MuiPaper: {
+      elevation1: {
+        boxShadow: 'none'
+      }
+    }
+  },
+  typography: {
+    useNextVariants: true
   }
 })
 
@@ -77,10 +75,10 @@ class MyRecipes extends Component {
   render() {
     const { classes, savedRecipes, deleteRecipe } = this.props
     const { expanded } = this.state
-    const { breakfast } = savedRecipes.filter(recipe => recipe.mealCategory === 'breakfast')
-    const { lunch } = savedRecipes.filter(recipe => recipe.mealCategory === 'Lunch')
-    const { dinner } = savedRecipes.filter(recipe => recipe.mealCategory === 'Dinner')
-    const { snack } = savedRecipes.filter(recipe => recipe.mealCategory === 'Snack')
+    const breakfast = savedRecipes.filter(recipe => recipe.mealCategory === 'Breakfast')
+    const lunch = savedRecipes.filter(recipe => recipe.mealCategory === 'Lunch')
+    const dinner = savedRecipes.filter(recipe => recipe.mealCategory === 'Dinner')
+    const snack = savedRecipes.filter(recipe => recipe.mealCategory === 'Snack')
     return (
       <Grid container
       alignItems="center"
@@ -88,22 +86,67 @@ class MyRecipes extends Component {
       spacing={40}
       className={classes.container}>
       <Typography variant="h3" align="center">My Recipes</Typography>
-      {savedRecipes.length > 0
-        ? <div className={classes.root}>
-          <ExpansionPanel expanded={expanded === 'breakfast'} onChange={this.handleChange('breakfast')}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Breakfast</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              {/* Breakfast items */}
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel expanded={expanded === 'lunch'} onChange={this.handleChange('lunch')}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Lunch</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-            {/* Lunch items */}
+      <div className={classes.root}>
+        <ExpansionPanel expanded={expanded === 'breakfast'} onChange={this.handleChange('breakfast')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Breakfast</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            {breakfast.length > 0
+              ? breakfast.map(item =>
+              <GridList className={classes.gridList} cols={3}>
+                <GridListTile key={item.recipeId}>
+                  <img src={item.image} alt={item.title} className={classes.image} />
+                  <GridListTileBar title={item.title}
+                    actionIcon={
+                    <i className={`material-icons ${classes.icon}`}
+                      id={item.recipeId}
+                      onClick={deleteRecipe}>clear</i>
+                    } />
+                </GridListTile>
+              </GridList>
+              )
+              : <MuiThemeProvider theme={theme}>
+                  <Card className={classes.messageContainer}>
+                    <CardContent>
+                      <Typography variant="h3"
+                      className={classes.message}>You do not have any saved breakfast recipes
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </MuiThemeProvider>
+            }
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'lunch'} onChange={this.handleChange('lunch')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Lunch</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+              {lunch.length > 0
+                ? lunch.map(item =>
+                <GridList className={classes.gridList} cols={3}>
+                  <GridListTile key={item.recipeId}>
+                    <img src={item.image} alt={item.title} className={classes.image} />
+                    <GridListTileBar title={item.title}
+                      actionIcon={
+                      <i className={`material-icons ${classes.icon}`}
+                        id={item.recipeId}
+                        onClick={deleteRecipe}>clear</i>
+                      } />
+                  </GridListTile>
+                </GridList>
+                )
+                : <MuiThemeProvider theme={theme}>
+                  <Card className={classes.messageContainer}>
+                    <CardContent>
+                      <Typography variant="h3"
+                      className={classes.message}>You do not have any saved lunch recipes
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </MuiThemeProvider>
+              }
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <ExpansionPanel expanded={expanded === 'dinner'} onChange={this.handleChange('dinner')}>
@@ -111,7 +154,30 @@ class MyRecipes extends Component {
               <Typography className={classes.heading}>Dinner</Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
-            {/* Dinner items */}
+              {dinner.length > 0
+                ? dinner.map(item =>
+                <GridList className={classes.gridList} cols={3}>
+                  <GridListTile key={item.recipeId}>
+                    <img src={item.image} alt={item.title} className={classes.image} />
+                    <GridListTileBar title={item.title}
+                      actionIcon={
+                      <i className={`material-icons ${classes.icon}`}
+                        id={item.recipeId}
+                        onClick={deleteRecipe}>clear</i>
+                      } />
+                  </GridListTile>
+                </GridList>
+                )
+                : <MuiThemeProvider theme={theme}>
+                  <Card className={classes.messageContainer}>
+                    <CardContent>
+                      <Typography variant="h3"
+                      className={classes.message}>You do not have any saved dinner recipes
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </MuiThemeProvider>
+              }
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <ExpansionPanel expanded={expanded === 'snack'} onChange={this.handleChange('snack')}>
@@ -119,42 +185,33 @@ class MyRecipes extends Component {
               <Typography className={classes.heading}>Snacks</Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
-            {/* Snack items */}
+              {snack.length > 0
+                ? snack.map(item =>
+                <GridList className={classes.gridList} cols={3}>
+                  <GridListTile key={item.recipeId}>
+                    <img src={item.image} alt={item.title} className={classes.image} />
+                    <GridListTileBar title={item.title}
+                      actionIcon={
+                      <i className={`material-icons ${classes.icon}`}
+                        id={item.recipeId}
+                        onClick={deleteRecipe}>clear</i>
+                      } />
+                  </GridListTile>
+                </GridList>
+                )
+                : <MuiThemeProvider theme={theme}>
+                  <Card className={classes.messageContainer}>
+                    <CardContent>
+                      <Typography variant="h3"
+                      className={classes.message}>You do not have any saved snack recipes
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </MuiThemeProvider>
+              }
             </ExpansionPanelDetails>
           </ExpansionPanel>
-          </div>
-
-
-
-        // ? <Grid item>
-        //   <List className={classes.list}>
-        //   {savedRecipes.map(recipe => {
-        //   return <Card className={classes.card} key={recipe.recipeId}>
-        //       <CardMedia className={classes.image}
-        //         image={recipe.image}
-        //         title="My Saved Recipe" />
-        //       <div className={classes.details}>
-        //         <CardContent className={classes.content}>
-        //           <ButtonBase href={`#view-recipe?id=${recipe.recipeId}`}>
-        //             <ListItemText primary={recipe.title} />
-        //           </ButtonBase>
-        //         </CardContent>
-        //         <i className={`material-icons ${classes.icon}`}
-        //         id={recipe.id}
-        //         onClick={deleteRecipe}>clear</i>
-        //       </div>
-        //     </Card>
-        //   })}
-        // </List>
-        // </Grid>
-        : <Card className={classes.messageContainer}>
-            <CardContent>
-              <Typography variant="h3"
-                className={classes.message}>You do not have any saved recipes
-              </Typography>
-            </CardContent>
-          </Card>
-        }
+        </div>
       </Grid>
     )
   }
